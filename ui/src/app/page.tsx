4100ef80368f021c6ecc6ecbe397a6b5165737d9
Martin Mwangi <mwangimartin1904@gmail.com>
2023-11-07 22:52:08 +0300

    update page with template components

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import HeroSection from "@/components/pages/home/HeroSection";
+import HeroSection from "@/components/pages/home/hero-section";
+import AboutSection from "@/components/pages/home/about-section";
+import ServicesSection from "@/components/pages/home/services-section";
+import FaqsSection from "@/components/pages/home/faq-section";
 
 const Home = () => {
   return (
     <div className="">
+
       <HeroSection />
+
+      <AboutSection />
+
+      <ServicesSection />
+
+      <FaqsSection />
+
     </div>
   );
 };
