6c0b25469a9d282f58009771c4aca6ecf440129d
Martin Mwangi <mwangimartin1904@gmail.com>
2023-11-07 22:51:55 +0300

    about section component

---
+++ ui/src/components/pages/home/about-section.tsx
@@ -0,0 +1,12 @@
+import SectionTitle from "@/components/ui/section-title";
+import React from 'react';
+
+const AboutSection = () => {
+	return (
+		<section className="mx-auto max-w-7xl p-6 md:py-8 lg:py-12 lg:px-8">
+			<SectionTitle title="About Us" />
+		</section>
+	);
+};
+
+export default AboutSection;
