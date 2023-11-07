24f4f4826dc1d04e732a3465a7942336adf0e700
Martin Mwangi <mwangimartin1904@gmail.com>
2023-11-07 23:24:11 +0300

    some updates

--- ui/src/components/pages/home/services-section.tsx
+++ ui/src/components/pages/home/services-section.tsx
@@ -4,7 +4,10 @@ import React from 'react';
 const ServicesSection = () => {
 	return (
 		<section className="mx-auto max-w-7xl p-6 md:py-8 lg:py-12 lg:px-8">
-			<SectionTitle title="Our Services" />
+			<SectionTitle
+				title="Our Services"
+				summary="We have 3+ years of experience in mobile and web app development that empowers businesses to strengthen their digital footprint in the highly competitive global marketplace."
+			/>
 		</section>
 	);
 };
