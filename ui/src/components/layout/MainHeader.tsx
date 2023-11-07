89b74c7b8a63862b1907aebda25b9f26393f9e8a
Martin Mwangi <mwangimartin1904@gmail.com>
2023-11-07 22:32:55 +0300

    add logo

--- ui/src/components/layout/MainHeader.tsx
+++ ui/src/components/layout/MainHeader.tsx
@@ -10,6 +10,7 @@ import {
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 const MainHeader = () => {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -18,10 +19,12 @@ const MainHeader = () => {
 		<header className="bg-white bottom_shadow fixed inset-x-0 top-0 z-[5]">
 			<nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
 				<div className="flex lg:flex-1">
-					<a href="#" className="-m-1.5 p-1.5">
+					<Link href="/" className="-m-1.5 p-1.5">
 						<span className="sr-only">GGL Tech</span>
-						<img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-					</a>
+						<div className="w-[60px] h-[60px]">
+							<Image width={80} height={80} className="!w-full !h-full object-cover" src="/images/logo.svg" alt="" />
+						</div>
+					</Link>
 				</div>
 				<div className="flex lg:hidden">
 					<button
@@ -61,14 +64,14 @@ const MainHeader = () => {
 				<div className="fixed inset-0 z-10" />
 				<Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 					<div className="flex items-center justify-between">
-						<a href="#" className="-m-1.5 p-1.5">
+						<Link href="/" className="-m-1.5 p-1.5">
 							<span className="sr-only">Your Company</span>
 							<img
 								className="h-8 w-auto"
 								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
 								alt=""
 							/>
-						</a>
+						</Link>
 						<button
 							type="button"
 							className="-m-2.5 rounded-md p-2.5 text-gray-700"
