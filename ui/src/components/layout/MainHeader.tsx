a44e25ceb9bb482a7c92ea47319e4d251832f17c
Martin Mwangi <mwangimartin1904@gmail.com>
2023-11-07 20:55:28 +0300

    add header

--- ui/src/components/layout/MainHeader.tsx
+++ ui/src/components/layout/MainHeader.tsx
@@ -1,41 +1,25 @@
 "use client";
 
-import { Fragment, useState } from 'react';;
-import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react';
+import { useState } from 'react';;
+import { Dialog } from '@headlessui/react';
 import {
-	ArrowPathIcon,
 	Bars3Icon,
-	ChartPieIcon,
-	CursorArrowRaysIcon,
-	FingerPrintIcon,
-	SquaresPlusIcon,
+	ChatBubbleLeftRightIcon,
 	XMarkIcon,
 } from '@heroicons/react/24/outline';
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid';
-import { cn } from "@/lib/utils";
 import Link from "next/link";
-
-const products = [
-	{ name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-	{ name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-	{ name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
-	{ name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-	{ name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
-];
-const callsToAction = [
-	{ name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-	{ name: 'Contact sales', href: '#', icon: PhoneIcon },
-];
+import { buttonVariants } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 const MainHeader = () => {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
 	return (
-		<header className="bg-white fixed inset-x-0 top-0 z-50">
+		<header className="bg-white bottom_shadow fixed inset-x-0 top-0 z-[5]">
 			<nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
 				<div className="flex lg:flex-1">
 					<a href="#" className="-m-1.5 p-1.5">
-						<span className="sr-only">Your Company</span>
+						<span className="sr-only">GGL Tech</span>
 						<img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
 					</a>
 				</div>
@@ -46,74 +30,30 @@ const MainHeader = () => {
 						onClick={() => setMobileMenuOpen(true)}
 					>
 						<span className="sr-only">Open main menu</span>
-						<Bars3Icon className="h-6 w-6" aria-hidden="true" />
+						<Bars3Icon className="h-8 w-8" aria-hidden="true" />
 					</button>
 				</div>
-				<Popover.Group className="hidden lg:flex lg:gap-x-12">
-					<Popover className="relative">
-						<Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-							Product
-							<ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-						</Popover.Button>
-
-						<Transition
-							as={Fragment}
-							enter="transition ease-out duration-200"
-							enterFrom="opacity-0 translate-y-1"
-							enterTo="opacity-100 translate-y-0"
-							leave="transition ease-in duration-150"
-							leaveFrom="opacity-100 translate-y-0"
-							leaveTo="opacity-0 translate-y-1"
-						>
-							<Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-								<div className="p-4">
-									{products.map((item) => (
-										<div
-											key={item.name}
-											className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-										>
-											<div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-												<item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
-											</div>
-											<div className="flex-auto">
-												<a href={item.href} className="block font-semibold text-gray-900">
-													{item.name}
-													<span className="absolute inset-0" />
-												</a>
-												<p className="mt-1 text-gray-600">{item.description}</p>
-											</div>
-										</div>
-									))}
-								</div>
-								<div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-									{callsToAction.map((item) => (
-										<a
-											key={item.name}
-											href={item.href}
-											className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-										>
-											<item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-											{item.name}
-										</a>
-									))}
-								</div>
-							</Popover.Panel>
-						</Transition>
-					</Popover>
-
-					<Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
-						Features
+				<div className="hidden lg:flex items-center lg:gap-x-10">
+					<Link href="/" className="font-semibold hover:border-b-[3px] hover:border-b-primary leading-6 text-gray-900">
+						Home
 					</Link>
-					<Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
-						Marketplace
+					<Link href="#portfolio" className="font-semibold hover:border-b-[3px] hover:border-b-primary leading-6 text-gray-900">
+						Portfolio
 					</Link>
-					<Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
-						Company
+					<Link href="#services" className="font-semibold hover:border-b-[3px] hover:border-b-primary leading-6 text-gray-900">
+						Services
 					</Link>
-				</Popover.Group>
+					<Link href="#team" className="font-semibold hover:border-b-[3px] hover:border-b-primary leading-6 text-gray-900">
+						Our Team
+					</Link>
+					<Link href="/about-us" className="font-semibold hover:border-b-[3px] hover:border-b-primary leading-6 text-gray-900">
+						About Us
+					</Link>
+				</div>
 				<div className="hidden lg:flex lg:flex-1 lg:justify-end">
-					<Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
-						Log in <span aria-hidden="true">&rarr;</span>
+					<Link href="/contact-us" className={cn(buttonVariants(), "text-lg flex gap-2")}>
+						<ChatBubbleLeftRightIcon className="w-6 h-6" />
+						Contact Us
 					</Link>
 				</div>
 			</nav>
@@ -135,59 +75,46 @@ const MainHeader = () => {
 							onClick={() => setMobileMenuOpen(false)}
 						>
 							<span className="sr-only">Close menu</span>
-							<XMarkIcon className="h-6 w-6" aria-hidden="true" />
+							<XMarkIcon className="h-8 w-8" aria-hidden="true" />
 						</button>
 					</div>
 					<div className="mt-6 flow-root">
 						<div className="-my-6 divide-y divide-gray-500/10">
 							<div className="space-y-2 py-6">
-								<Disclosure as="div" className="-mx-3">
-									{({ open }) => (
-										<>
-											<Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-												Product
-												<ChevronDownIcon
-													className={cn(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
-													aria-hidden="true"
-												/>
-											</Disclosure.Button>
-											<Disclosure.Panel className="mt-2 space-y-2">
-												{[...products, ...callsToAction].map((item) => (
-													<Disclosure.Button
-														key={item.name}
-														as="a"
-														href={item.href}
-														className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-													>
-														{item.name}
-													</Disclosure.Button>
-												))}
-											</Disclosure.Panel>
-										</>
-									)}
-								</Disclosure>
 								<Link
 									href="#"
 									className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 								>
-									Features
+									Home
 								</Link>
 								<Link
-									href="#"
+									href="#portfolio"
 									className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 								>
-									Marketplace
+									Portfolio
 								</Link>
 								<Link
-									href="#"
+									href="#services"
 									className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 								>
-									Company
+									Services
+								</Link>
+								<Link
+									href="#team"
+									className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+								>
+									Our Team
+								</Link>
+								<Link
+									href="/about-us"
+									className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+								>
+									About Us
 								</Link>
 							</div>
 							<div className="py-6">
 								<Link
-									href="#"
+									href="/contact-us"
 									className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 								>
 									Contact Us
