2ac76ef1d25d3625e7f57c96b741da4f948c34c3
Martin Mwangi <mwangimartin1904@gmail.com>
2023-11-07 22:51:42 +0300

    service section component

---
+++ ui/src/components/pages/home/services-section.tsx
@@ -0,0 +1,12 @@
+import SectionTitle from "@/components/ui/section-title";
+import React from 'react';
+
+const ServicesSection = () => {
+	return (
+		<section className="mx-auto max-w-7xl p-6 md:py-8 lg:py-12 lg:px-8">
+			<SectionTitle title="Our Services" />
+		</section>
+	);
+};
+
+export default ServicesSection;
